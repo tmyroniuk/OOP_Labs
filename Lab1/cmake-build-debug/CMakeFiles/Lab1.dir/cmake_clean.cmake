b2df761c8855104ac59e786f2780c113b40237ee
TNhuntet <tmyroniuk@gmail.com>
2019-09-25 12:53:11 +0300

    Initial commit

---
+++ Lab1/cmake-build-debug/CMakeFiles/Lab1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab1.dir/main.cpp.obj"
+  "Lab1.pdb"
+  "Lab1.exe"
+  "Lab1.exe.manifest"
+  "Lab1.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ OOP1/cmake-build-debug/CMakeFiles/OOP1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP1.dir/main.cpp.obj"
+  "OOP1.pdb"
+  "OOP1.exe"
+  "OOP1.exe.manifest"
+  "OOP1.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
